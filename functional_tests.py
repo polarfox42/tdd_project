45978b36ddd374726dad71387307a38cff9a1346
polarfox42 <vg.mkrtchyan@gmail.com>
2020-04-15 17:27:55 +0500

    FT now checks we can input a to-do item

--- functional_tests.py
+++ functional_tests.py
@@ -1,4 +1,6 @@
 from selenium import webdriver
+from selenium.webdriver.common.keys import Keys
+import time
 import unittest
 
 class NewVisitorTest(unittest.TestCase):
@@ -10,23 +12,39 @@ class NewVisitorTest(unittest.TestCase):
         self.browser.quit()
 
     def test_can_start_a_list_and_retrieve_it_later(self):
-        '''тест может начать список и получить его позже'''
+        '''можно начать список и получить его позже'''
         # Лиса хочет опробовать крутое приложение для составления списка дел
         self.browser.get('http://localhost:8000')
 
         # Заголовок и шапка страницы говорят о списках неотложных дел.
         self.assertIn('To-Do', self.browser.title)
-        self.fail('Закончить тест!')
+        header_text = self.browser.find_element_by_tag_name('h1').text
+        self.assertIn('To-Do', header_text)
 
         # Ей сразу предлагается ввести элемент списка
+        inputbox = self.browser.find_element_by_id('id_new_item')
+        self.assertEqual(
+            inputbox.get_attribute('placeholder'),
+            'Enter a to-do item'
+        )
 
         # Она набирает в текстовом поле "Купить павлиньи перья"
+        inputbox.send_keys('Купить павлиньи перья')
 
         # Когда она нажимает enter, страница обновляется, и теперь страница
         # содержит "1: Купить павлиньи перья" в качестве элемента списка
+        inputbox.send_keys(Keys.ENTER)
+        time.sleep(1)
+
+        table = self.browser.find_element_by_id('id_list_table')
+        rows = table.find_elements_by_tag_name('tr')
+        self.assertTrue(
+            any(row.text == '1. Купить павлиньи перья' for row in rows)
+        )
 
         # Текстовое поле по-прежнему приглашает ее добавить еще один элемент.
         # Она вводит "Сделать блесну из павлиньих перьев"
+        self.fail('Закончить тест!')
 
         # Страница снова обновляется, и теперь показывает оба элемента списка
 
