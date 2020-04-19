e12e4fc535dd3fc536197ce271b382764a79a823
polarfox42 <vg.mkrtchyan@gmail.com>
2020-04-19 16:53:04 +0500

    add new test to handle multiple users work

--- functional_tests/tests.py
+++ functional_tests/tests.py
@@ -1,8 +1,11 @@
 from django.test import LiveServerTestCase
 from selenium import webdriver
 from selenium.webdriver.common.keys import Keys
+from selenium.common.exceptions import WebDriverException
 import time
 import unittest
+
+MAX_WAIT = 10
 
 class NewVisitorTest(LiveServerTestCase):
 
@@ -12,11 +15,19 @@ class NewVisitorTest(LiveServerTestCase):
     def tearDown(self):
         self.browser.quit()
 
-    def check_for_row_in_list_table(self, row_text):
-        table = self.browser.find_element_by_id('id_list_table')
-        rows = table.find_elements_by_tag_name('tr')
-        self.assertIn(row_text, [row.text for row in rows],
-            f'Новый элемент списка не появился в таблице. Содержимым было:\n{table.text}')
+    def wait_for_row_in_list_table(self, row_text):
+        start_time = time.time()
+        while True:
+            try:
+                table = self.browser.find_element_by_id('id_list_table')
+                rows = table.find_elements_by_tag_name('tr')
+                self.assertIn(row_text, [row.text for row in rows],
+                    f'Новый элемент списка не появился в таблице. Содержимым было:\n{table.text}')
+                return
+            except (AssertionError, WebDriverException) as e:
+                if time.time() - start_time > MAX_WAIT:
+                    raise e
+                time.sleep(0.5)
 
     def test_can_start_a_list_and_retrieve_it_later(self):
         '''можно начать список и получить его позже'''
@@ -41,19 +52,17 @@ class NewVisitorTest(LiveServerTestCase):
         # Когда она нажимает enter, страница обновляется, и теперь страница
         # содержит "1: Купить павлиньи перья" в качестве элемента списка
         inputbox.send_keys(Keys.ENTER)
-        time.sleep(1)
-        self.check_for_row_in_list_table('1: Купить павлиньи перья')
+        self.wait_for_row_in_list_table('1: Купить павлиньи перья')
 
         # Текстовое поле по-прежнему приглашает ее добавить еще один элемент.
         # Она вводит "Сделать блесну из павлиньих перьев"
         inputbox = self.browser.find_element_by_id('id_new_item')
         inputbox.send_keys('Сделать блесну из павлиньих перьев')
         inputbox.send_keys(Keys.ENTER)
-        time.sleep(1)
 
         # Страница снова обновляется, и теперь показывает оба элемента списка
-        self.check_for_row_in_list_table('1: Купить павлиньи перья')
-        self.check_for_row_in_list_table('2: Сделать блесну из павлиньих перьев')
+        self.wait_for_row_in_list_table('1: Купить павлиньи перья')
+        self.wait_for_row_in_list_table('2: Сделать блесну из павлиньих перьев')
 
         # Лиса видит, что сайт сгенерировал для нее уникальный урл-адрес - об этом
         # выводится небольшой текст с объяснениями
@@ -62,6 +71,48 @@ class NewVisitorTest(LiveServerTestCase):
         # Она посещает этот адрес и убеждается, что ее список по-прежнему там.
 
         # Лиса закрывает браузер и возвращается к своим делам.
+
+    def test_multiple_users_can_start_lists_at_different_urls(self):
+        # Лиса начинает новый список
+        self.browser.get(self.live_server_url)
+        inputbox = self.browser.find_element_by_id('id_new_item')
+        inputbox.send_keys('Купить павлиньи перья')
+        inputbox.send_keys(Keys.ENTER)
+        self.wait_for_row_in_list_table('1: Купить павлиньи перья')
+
+        # Она замечает, что ее список имеет уникальный адрес
+        lisa_list_url = self.browser.current_url
+        self.assertRegex(lisa_list_url, '/lists/.+')
+
+        # Теперь новый пользователь, Ник, приходит на сайт
+        ## Новый сеанс браузера, чтобы никакая инфа не просочилась
+        ## через куки
+        self.browser.quit()
+        self.browser = webdriver.Firefox()
+
+        # Ник посещает домашнюю страницу. Нет никаких признаков списка Лисы
+        self.browser.get(self.live_server_url)
+        page_text = self.browser.find_element_by_tag_name('body').text
+        self.assertNotIn('Купить павлиньи перья', page_text)
+        self.assertNotIn('Сделать блесну', page_text)
+
+        # Ник начинает новый список, вводя новый элемент
+        inputbox = self.browser.find_element_by_id('id_new_item')
+        inputbox.send_keys('Купить молоко')
+        inputbox.send_keys(Keys.ENTER)
+        self.wait_for_row_in_list_table('1: Купить молоко')
+
+        # Ник получает уникальный адрес списка
+        nick_list_url = self.browser.current_url
+        self.assertRegex(nick_list_url, '/lists/.+')
+        self.assertNotEqual(nick_list_url, lisa_list_url)
+
+        # Ни следа списка Лисы
+        page_text = self.browser.find_element_by_tag_name('body').text
+        self.assertNotIn('Купить павлиньи перья', page_text)
+        self.assertIn('Купить молоко', page_text)
+
+        # Довольные, оба покидают сайт.
 
 if __name__ == '__main__':
     unittest.main(warnings='ignore')
