460334f7029af44ba04a0839682f7b54020a42d7
polarfox42 <vg.mkrtchyan@gmail.com>
2020-04-19 21:26:46 +0500

    new url, view and template to display lists

--- functional_tests/tests.py
+++ functional_tests/tests.py
@@ -21,8 +21,7 @@ class NewVisitorTest(LiveServerTestCase):
             try:
                 table = self.browser.find_element_by_id('id_list_table')
                 rows = table.find_elements_by_tag_name('tr')
-                self.assertIn(row_text, [row.text for row in rows],
-                    f'Новый элемент списка не появился в таблице. Содержимым было:\n{table.text}')
+                self.assertIn(row_text, [row.text for row in rows])
                 return
             except (AssertionError, WebDriverException) as e:
                 if time.time() - start_time > MAX_WAIT:
