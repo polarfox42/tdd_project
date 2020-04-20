e569e110db5584ca6a719010f25ba2b6d6af104a
polarfox42 <vg.mkrtchyan@gmail.com>
2020-04-20 19:04:52 +0500

    add list model and connect each item to list

--- lists/models.py
+++ lists/models.py
@@ -1,6 +1,12 @@
 from django.db import models
 
-# Create your models here.
+
+class List(models.Model):
+    '''To-Do list'''
+    pass
+
+
 class Item(models.Model):
     '''List item'''
     text = models.TextField(default='')
+    list = models.ForeignKey(List, default=None, on_delete=models.PROTECT)
